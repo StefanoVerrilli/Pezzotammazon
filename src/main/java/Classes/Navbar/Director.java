f5cd8fadd28af1dbb0fa287faeeb02f1b1bec76e
frapgit99 <fehan56125@bulkbye.com>
2022-07-03 20:20:44 +0200

    Risolto errore

--- src/main/java/Classes/Navbar/Director.java
+++ src/main/java/Classes/Navbar/Director.java
@@ -7,15 +7,18 @@ public class Director {
 
     private BuilderInterface Builder;
 
-    public void constructAdminNavBar(BuilderInterface builder){
-        builder.addLink(new Pair<String,String>("Inserisci prodotto","InsertLogic.do"));
-        builder.addLink(new Pair<String,String>("Visualizza prodotti","ProductsTable.do"));
-        builder.addLink(new Pair<String,String>("Analysis","UsersPageLogic.do"));
+    public Director(BuilderInterface builder) {
+        this.Builder = builder;
+    }
+    public void constructAdminNavBar(){
+        Builder.addLink(new Pair<String,String>("Inserisci prodotto","InsertLogic.do"));
+        Builder.addLink(new Pair<String,String>("Visualizza prodotti","ProductsTable.do"));
+        Builder.addLink(new Pair<String,String>("Analysis","UsersPageLogic.do"));
     }
 
-    public void constructUserNavBar(BuilderInterface builder){
-        builder.addLink(new Pair<String,String>("Carrello","CartLogic.do"));
-        builder.addLink(new Pair<String,String>("Visualizza prodotti","ProductsTable.do"));
+    public void constructUserNavBar(){
+        Builder.addLink(new Pair<String,String>("Carrello","CartLogic.do"));
+        Builder.addLink(new Pair<String,String>("Visualizza prodotti","ProductsTable.do"));
     }
 
 }
